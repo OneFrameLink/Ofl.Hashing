1bf427e41c497317c884ad3a6babea0f8f3b8e1a
casperOne <casperOne@caspershouse.com>
2017-05-20 00:54:22 -0400

    Partial implementation of the FNV1A algortithm.

---
+++ src/Ofl.Hashing/FowlerNollVo1A/FowlerNollVoHash1AAlgorithm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Numerics;
+
+namespace Ofl.Hashing.FowlerNollVo1A
+{
+    public class FowlerNollVoHash1AAlgorithm : IHashAlgorithm
+    {
+        #region Constructor
+
+        public FowlerNollVoHash1AAlgorithm(FowlerNollVoHash1AAlgorithmParameters parameters)
+        {
+            // Validate parameters.
+            _parameters = parameters ?? throw new ArgumentNullException(nameof(parameters));
+
+            // Assign the values.
+        }
+
+        #endregion
+
+        #region Instance state.
+
+        private readonly FowlerNollVoHash1AAlgorithmParameters _parameters;
+
+        private BigInteger? _current;
+
+        #endregion
+
+        #region IHashAlgorithm implementation.
+
+        public int HashSize => _parameters.Bytes;
+
+        public void TransformBlock(byte[] bytes, int offset, int count)
+        {
+            // Create the array segment.
+            // NOTE: This validates parameters.
+            var segment = new ArraySegment<byte>(bytes, offset, count);
+
+            // The current hash.
+            BigInteger currentHash = _current ?? _parameters.Offset;
+
+            // Aggregate.
+            _current = segment.Aggregate(currentHash, (current, b) => ((current ^ b).Unchecked(HashSize) * _parameters.Prime).Unchecked(HashSize));
+        }
+
+        public IReadOnlyCollection<byte> GetRunningHash() =>
+            _current == null ? null : new ReadOnlyCollection<byte>(_current.Value.ToByteArray());
+
+        #endregion
+
+        #region IDisposable implementation.
+
+        public void Dispose()
+        {
+            // Call the overload and
+            // suppress finalization.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Dispose of unmanaged resources here.
+
+            // If not disposing, get out.
+            if (!disposing) return;
+
+            // Dispose of IDisposable implementations.
+        }
+
+        ~FowlerNollVoHash1AAlgorithm()
+        {
+            // Call the overload, not disposing.
+            Dispose(false);
+        }
+
+        #endregion
+    }
+}
---
+++ test/Ofl.Hashing.Tests/BigIntegerExtensons.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Numerics;
+using Xunit;
+
+namespace Ofl.Hashing.Tests
+{
+    public class BigIntegerExtensons
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void Test_Unchecked_Positive(int iterations)
+        {
+            // Seed the big int and the int.
+            int intValue = Int32.MaxValue;
+            BigInteger bigint = intValue;
+
+            // Allow overflow.
+            unchecked
+            {
+                // Loop.
+                for (int index = 0; index < iterations; ++index)
+                {
+                    // Add two and int 32 min.
+                    intValue += 2;
+                    bigint = (bigint + new BigInteger(2)).Unchecked(32);
+
+                    // Possible overflow.
+                    Assert.Equal(intValue, bigint);
+
+                    // Definite overflow.
+                    intValue += Int32.MaxValue;
+                    bigint = (bigint + new BigInteger(Int32.MaxValue)).Unchecked(32);
+
+                    // Assert.
+                    Assert.Equal(intValue, bigint);
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void Test_Unchecked_Negative(int iterations)
+        {
+            // Seed the big int and the int.
+            int intValue = Int32.MinValue;
+            BigInteger bigint = intValue;
+
+            // Allow overflow.
+            unchecked
+            {
+                // Loop.
+                for (int index = 0; index < iterations; ++index)
+                {
+                    // Add two and int 32 min.
+                    intValue -= 2;
+                    bigint = (bigint - new BigInteger(2)).Unchecked(32);
+
+                    // Possible overflow.
+                    Assert.Equal(intValue, bigint);
+
+                    // Definite overflow.
+                    intValue -= Int32.MaxValue;
+                    bigint = (bigint - new BigInteger(Int32.MaxValue)).Unchecked(32);
+
+                    // Assert.
+                    Assert.Equal(intValue, bigint);
+                }
+            }
+        }
+    }
+}
